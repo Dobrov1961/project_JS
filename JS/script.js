f06db3455a7bca63efe9b63ae3012dadb6fb2a60
Andrii <Andrew.dobrovv@gmail.com>
2020-12-01 13:13:28 +0200

    My first commit

---
+++ JS/script.js
@@ -0,0 +1,100 @@
+"use strict"
+// let number = 4.6;
+
+// console.log(4/0);
+// console.log('4ggh'/0);
+//  const persone = "Andrii";
+
+//  let bool = true/false;
+
+
+
+//  console.log(nub);
+
+//  const obj = {
+//      name: 'Danya',
+//      age: 24,
+//      isMarried: false,
+//  }
+//  console.log(obj.name);
+//  console.log(obj['name']); 
+
+
+//  let array = ['plum.png', 'orange.jpeg', 'mu.raw', [245, 'fmfmg'], {}];
+//  console.log(array[3.0]);
+
+// alert('Hello world');
+// let result = confirm('How old are you?');
+// console.log(result);
+
+// let result = +prompt ('You are more than 18?',  '678');
+// console.log(result + 5);
+
+// const answers = [];
+
+// answers[0] = 'Danya';
+// answers[1] = 24;
+//  console.log(answers);
+
+// const answers = [];
+
+// answers[0] = (prompt('Do you have child?', 'Yes/No'));
+// answers[1] = (prompt('what is his/her name ', ''));
+// answers[2] = (prompt('his/her has child?', 'Yes/No'));
+
+// document.write(answers);
+// console.log(typeof(answers));
+
+// const arr = [
+//     'dklvnkljvlnvd',
+//     [
+//         'here is me',
+//         'here is you',
+//     ],
+//     [
+//         'some str',
+//         'some num',
+//         'some qqq',
+//     ]
+// ];
+// console.log(arr[2][0]);
+
+// console.log(123);
+// // alert(123);
+// console.log(123);
+// // alert(321);
+// console.log(321);
+// document.write('asdasd');
+
+// const category = 'Toys';
+
+// console.log('My toys '+ 'Max`s' + category );
+// console.log(`MAX HAS  ${category}`);
+
+// console.log(5+ +'5');
+
+// let incr = 10,
+//     decr = 10,
+//     incrs = 10,
+//     decrs = 10;
+
+//     incr++;
+//     decr--;
+
+//     ++incrs; 
+//     --decrs;
+
+//     console.log(incr);
+//     console.log(decr);
+//     console.log(incrs);
+//     console.log(decrs);
+
+const isChecked = true;
+const isClose = false;
+
+console.log(isChecked && isClose);
+
+
+
+
+
