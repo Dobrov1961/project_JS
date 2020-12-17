56cee1289d61f624d73d0ea655071ffa0c842aff
Andrii <Andrew.dobrovv@gmail.com>
2020-12-17 21:21:10 +0200

    Commit 3 lesson

--- JS/script.js
+++ JS/script.js
@@ -1,4 +1,4 @@
-"use strict"
+// "use strict"
 // let number = 4.6;
 
 // console.log(4/0);
@@ -249,7 +249,251 @@
 
 // console.log(Math.round(number));
 
-const test = "12.2px";
-console.log(parseInt(test));
-console.log(parseFloat(test));
+// const test = "12.2px";
+// console.log(parseInt(test));
+// console.log(parseFloat(test));
 
+
+// function first() {
+//     setTimeout(function() {
+//         console.log(1);
+//     }, 500);
+// }
+
+// function second() {
+//     console.log(2);
+// }
+
+// first();
+// second();
+// function learnJS(lang, callback) {
+//     console.log(`I learn ${lang}`);
+//     callback(); 
+// }
+
+// function done() {
+//     console.log('I have done this lesson');
+// }
+
+// function nedone() {
+//     console.log('I have not done this lesson');
+// }
+
+// learnJS('English', done);
+// learnJS('Math', nedone);
+
+
+
+
+// learnJS(English, function() {
+//     console.log('I have done this lesson')
+// }); 
+
+// const option = {
+//     name: 'test',
+//     width: 1024,
+//     height: 1024,
+//     colors: {
+//         border: 'black',
+//         bg: 'red'
+//     },
+//     makeTest: function() {
+//         console.log('test');
+//     }
+// };
+
+// option.makeTest();
+
+// // const {border, bg} = option.colors;
+// const {border, bg} = option['colors'];
+// console.log(bg);
+// console.log(option.name);
+// ['border']
+// delete option.name;
+ 
+// console.log(option['colors']['border']);
+// let count = 0;
+
+// for (let key in option ){
+//     if(typeof(option[key]) === 'object'){
+//             for (let i in option[key]){ 
+//                 console.log(`Свойства ${i} имеет значение ${option[key][i]}`)
+                
+//             } 
+//         } else {
+//             console.log(`Свойства ${key} имеет значение ${option[key]}`)
+           
+//     }  
+// }
+// count++;
+// console.log(count);
+
+// console.log(Object.keys(option).length);
+
+
+// let OBJ = new Object();
+
+// console.log(OBJ);
+
+// Масиви
+
+// const arr = [1345, 386, 15, 767, 8098];
+// console.log(arr.sort(compareNum));
+
+// function compareNum(a, b){
+//     return a - b;
+// }
+// arr.forEach(function (item, i, arr){
+//     console.log(`${i}: ${item} in arr ${arr}`);
+// });
+
+// const str = prompt(' ', '');
+// const products = str.split(', ');
+// products.sort();
+// console.log(products.join('; '));
+
+
+
+
+// console.log(arr);
+
+// for (let i = 1; i < arr.length;i++){
+//     console.log(arr[i]);
+// }
+
+// console.log(arr[3]);
+
+// for (let value of arr){
+//     console.log(value);
+// }
+
+// arr[99] = 0;
+// console.log(arr.length);
+// console.log(arr);
+
+// let a = 5;
+// let b = a;
+
+// b = b + 5;
+// console.log(b);
+// console.log(a);
+
+// let obj = {
+//     a: 5,
+//     b: 1
+// };
+
+// let copy = obj;  
+
+// copy.a = 10;
+// console.log(copy);
+// function copy(mainObj){
+//     let objCopy = {};
+//     let key; 
+//     for(key in mainObj){
+//         objCopy[key] = mainObj[key];
+        
+//     } 
+//     return objCopy;
+// }
+
+// console.log(obj);
+
+// const numbers = {
+//     a: 5,
+//     b: 7,
+//     c: {
+//         x: 9,
+//         y: 11
+//     }
+// };
+ 
+// const newNumbers = copy(numbers);
+
+// numbers.c.y = 12;
+// newNumbers.a = 10;
+// console.log(numbers);
+// console.log(newNumbers);
+ 
+
+// let add = {
+//     n: 3,
+//     m: 6
+// };
+
+// console.log(Object.assign(numbers, add));
+// let newObj = Object.assign({}, add);
+// newObj.n = 6;
+// console.log(newObj);
+
+// let oldArr = ['a', 'b', 'c'];
+// let newArr = oldArr.slice();
+
+// newArr[1] = 'dsva lfdm';
+// console.log(newArr);
+// console.log(oldArr);
+
+// const video = ['Youtube.com', 'twitch.com', 'vimeo'];
+// const blogs = ['lifejornal', 'insta'];
+// let internet = [...video, ...blogs, 'vk', 'facebook'];
+// console.log(internet);
+// console.log(...video);
+// console.log(...blogs);
+
+// function log(a, b, c){
+//     console.log(a);
+//     console.log(b);
+//     console.log(c);
+// }
+
+// const num = [6, 7, 3];
+
+// log(...num);
+
+// let someArr = ['Novus', 'Silpo', 'Atb', 'eko'];
+
+// console.log(someArr);
+// console.log(...someArr);
+
+// let someObj = {
+//     a: 'Novus',
+//     b:'Silpo',  
+//     c:'Atb',
+//     d:'eko'
+// };
+
+
+// let modeObj = {...someObj};
+// console.log(modeObj);
+
+// let q = {
+//     one: 1,
+//     two: 2
+// };
+// let newQ = {...q};
+// console.log(newQ);
+
+// let str = 'some';
+// let strObj = new String(str);
+
+// console.log(typeof(str));
+// console.log(typeof(strObj));
+
+const soldier = {
+    health: 400,
+    armor: 100
+};
+
+// const jonh = {
+//     health: 100,
+// }
+
+const jonh = Object.create(soldier);
+console.log(jonh.health);
+
+
+// jonh.__proto__ = soldier;
+
+// Object.setPrototypeOf(jonh, soldier)
+
+// console.log(jonh.armor);
